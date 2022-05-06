4e1fd1d255682fa00f570a20a10c3ef7f746f78f
MitchScho <scho.mitch@gmail.com>
2022-05-06 21:35:11 +0000

    refactor addProperty function query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -168,7 +168,7 @@ exports.getAllProperties = getAllProperties;
 const addProperty = function (property) {
   const params = [property.owner_id, property.title, property.description,
     property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night,
-    property.stree, property.city, property.province, property.post_code, property.country,
+    property.street, property.city, property.province, property.post_code, property.country,
     property.parking_spaces, property.number_of_bathrooms,property.number_of_bedrooms];
   const queryString = `
                       INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, 
